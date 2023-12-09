85f4205393cf8067a09a3732c794af56f7bed5b2
DzKriMo <amamra.mehdiakram@gmail.com>
2023-12-09 23:00:07 +0100

    fixed barcode

--- app/src/main/java/com/thekrimo/resilience/BoycottedActivity.kt
+++ app/src/main/java/com/thekrimo/resilience/BoycottedActivity.kt
@@ -1,6 +1,16 @@
 package com.thekrimo.resilience
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.thekrimo.resilience.databinding.ActivityBarcodeBinding
+import com.thekrimo.resilience.databinding.ActivityBoycottedBinding
 
 class BoycottedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBoycottedBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBoycottedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
 }
